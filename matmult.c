5cb301cce1fe2ab61d634108567de14ece82627c
Amadou Agne <amag7207@rackham2.uppmax.uu.se>
2017-04-07 17:08:12 +0200

    working on fox algorithm

--- matmult.c
+++ matmult.c
@@ -3,19 +3,54 @@
 #include <stdlib.h>
 #include <math.h>
 
+
+// Function to perform the step 2 of Fox's algorithm
+void Block_matmul(int *subA, int *SubB, int *SubC, int block_size){
+     int i,j,k;
+     for(i=0;i<block_size;i++){
+        for(k=0;k<block_size;k++){
+           for(j=0;j<block_size;j++){ // efficient matrix multiplication
+              subC[i][j]+=subA[i][k]*subB[k][j];
+           } 
+        }
+     }     
+}
+
+
+
+
+
 int main(int argc, char *argv[]) {
   
-    int rank, size, mat_size, block_size, i, j, p_x, dest;
+    int rank, size, mat_size, block_size, i, j,k, p_x,p_y, dest;
     double A[mat_size][mat_size];
     double B[mat_size][mat_size];
     double C[mat_size][mat_size];
+    double *subA;
+    double *subB;
+    double *subC;
+    int dims[2] = {0,0}, periods[2] = {1,1}, reorder = 1; 
+    
+    // create communicators
+    MPI_Comm row_comm;
+    MPI_Comm col_comm;
+    MPI_Comm block_comm;
+
   
+    MPI_Datatype block;
     MPI_Status status;
+
 
     MPI_Init(&argc, &argv);       
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   
+    
+     /* Create a virtual 2D-grid topology */
+    MPI_Dims_create(size, 2, dims);  
+    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, reorder, &block_comm); 
+    MPI_Comm_rank(block_comm, &rank); 
+    
     mat_size = atoi(argv[1]);
 	/* Generate random Matrices */
     if (rank == 0){
@@ -44,6 +79,13 @@ int main(int argc, char *argv[]) {
 		//MPI_Recv () blocks 
 	
 		/* Apply fox algorithm on blocks */
+              
+              //1. broadcast Aim
+              //2. call block_matmul
+              //3. shift the B blocks
+
+
+               fox();
 	
 	
 		/* Send result */
