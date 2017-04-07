d2e237bd482713f261d32e1f5ab6caf341f276c6
maba03 <madeleine.bailleux@telecom-bretagne.eu>
2017-04-07 15:15:57 +0200

    Update prog.c

--- prog.c
+++ prog.c
@@ -51,8 +51,8 @@ int main(int argc, char *argv[]) {
 
 	int disps[PROC];
 	int counts[PROC];
-	for (int j=0; j<2; j++) {
-		for (int k=0; k<2; k++) {
+	for (int j=0; j<SPROC; j++) {
+		for (int k=0; k<SPROC; k++) {
 			disps[j*SPROC+k] = j*SIZE*BLOCKS+k*BLOCKS;
 			counts [j*SPROC+k] = 1;
 		}
